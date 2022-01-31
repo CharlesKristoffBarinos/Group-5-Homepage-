3a1a71f60b49ec50fb55eadf98c41ab315973c91
CharlesKristoffBarinos <98002704+CharlesKristoffBarinos@users.noreply.github.com>
2022-01-31 20:50:15 +0800

    Update index.php

--- index.php
+++ index.php
@@ -14,12 +14,12 @@
    <table width="1000" height="50" border="1" bordercolor="black"> 
     <tbody> 
      <tr> 
-      <th background="Math.jpg"> <font size="40">Math <br><br> Subject Teacher <br><img src="Janelle.jpg" width="250" height="250"><br>Miss. Janelle Javellana <br> <a href="MATH.html">Math </a></font></th> 
+      <th background="MATH9.jpg"> <font size="40">Math <br><br> Subject Teacher <br><img src="Janelle.jpg" width="250" height="250"><br>Miss. Janelle Javellana <br> <a href="MATH.html">Math </a></font></th> 
       <th background="Cle.jpg"><font size="40">CLE <br><br> Subject Teacher <br><img src="Lili.jpg" width="250" height="250"><br> Miss. Liliveth Gustilo <br> <a href="CLE.html">CLE</a></font></th> 
      </tr> 
      <tr> 
       <th background="Cae.jpg"><font size="40">CAE <br> <br>Subject Teacher <br><img src="SirV.jpg" width="250" height="250"><br>Mr. Ariel Villaceran <br> <a href="CAE.html">CAE</a> </font></th> 
-      <th background="Tle.jpg"><font size="40"> TLE <br><br> Subject Teacher <br><img src="Mae.jpg" width="250" height="250"><br>Miss. Mae Carmelle Bancolo <br><a href="TLE.html">Rubrics</a></font> </th> 
+      <th background="TLE.jpg"><font size="40"> TLE <br><br> Subject Teacher <br><img src="Mae.jpg" width="250" height="250"><br>Miss. Mae Carmelle Bancolo <br><a href="TLE.html">Rubrics</a></font> </th> 
      </tr> 
     </tbody> 
    </table> 
