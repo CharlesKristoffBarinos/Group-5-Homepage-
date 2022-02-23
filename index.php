01fc68007e6a32ff0938498b5d47fd3a9a66e52c
CharlesKristoffBarinos <98002704+CharlesKristoffBarinos@users.noreply.github.com>
2022-02-23 12:16:49 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,32 +1,31 @@
-<!doctype html>
 <html> 
  <head> 
   <title>Homepage</title> 
  </head> 
- <body align="center" background="Animo.jpg"> 
+ <body align="center" background="WebPage.png"> 
   <br> 
-  <img src="Helloandwel.jpg" width="1000" height="250" align="center"> 
+  <img src="Hello.jpg" width="1000" height="250" align="center"> 
   <br> 
   <br> 
   <br> 
   <br> 
   <center> 
-   <table width="1000" height="50" border="5" bordercolor="black"> 
+   <table width="1000" height="50" border="1" bordercolor="black"> 
     <tbody> 
      <tr> 
-      <th background="MATH9.jpg"> <font size="40">Math <br><br> Subject Teacher <br><img src="Janelle.jpg" width="250" height="250"><br>Miss. Janelle Javellana <br> <a href="MATH.html">Math </a></font></th> 
-      <th background="Cle.jpg"><font size="40">CLE <br><br> Subject Teacher <br><img src="Lili.jpg" width="250" height="250"><br> Miss. Liliveth Gustilo <br> <a href="CLE.html">CLE</a></font></th> 
+      <th background="MATHBACKGROUND.png"> <font size="40">Math <br><br> Subject Teacher <br><img src="Janelle.jpg" width="250" height="250"><br>Miss. Janelle Javellana <br> <a href="FramesMath.html">Math </a></font></th> 
+      <th background="CLEBACKGROUND.png"><font size="40">CLE <br><br> Subject Teacher <br><img src="Lili.jpg" width="250" height="250"><br> Miss. Liliveth Gustilo <br> <a href="CLE.html">CLE</a></font></th> 
      </tr> 
      <tr> 
-      <th background="Cae.jpg"><font size="40">CAE <br> <br>Subject Teacher <br><img src="SirV.jpg" width="250" height="250"><br>Mr. Ariel Villaceran <br> <a href="CAE.html">CAE</a> </font></th> 
-      <th background="9Ctle.jpg"><font size="40"> TLE <br><br> Subject Teacher <br><img src="Mae.jpg" width="250" height="250"><br>Miss. Mae Carmelle Bancolo <br><a href="TLE.html">Rubrics</a></font> </th> 
+      <th background="CAEBACKGROUND.png"><font size="40">CAE <br> <br>Subject Teacher <br><img src="SirV.jpg" width="250" height="250"><br>Mr. Ariel Villaceran <br> <a href="CAE.html">CAE</a> </font></th> 
+      <th background="TLEBACKGROUND.png"><font size="40"> TLE <br><br> Subject Teacher <br><img src="Mae.jpg" width="250" height="250"><br>Miss. Mae Carmelle Bancolo <br><a href="TLE.html">Rubrics</a></font> </th> 
      </tr> 
     </tbody> 
    </table> 
    <br> 
    <br> 
    <br> 
-   <table width="550" height="100" border="5" bordercolor="black" bgcolor="white"> 
+   <table width="550" height="100" border="5" bordercolor="red" bgcolor="#89dd98"> 
     <tbody> 
      <tr> 
       <th>Group Members</th> 
